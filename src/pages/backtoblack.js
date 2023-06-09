bc1231fbc196e68dad15dc48ee59407b4a8a0d67
vicma <vmaldaraca@gmail.com>
2023-06-09 15:25:52 -0600

    Fix black to back component and all console errors

--- src/pages/backtoblack.js
+++ src/pages/backtoblack.js
@@ -7,7 +7,7 @@ import Nod from '../components/nod';
 export default function TheStripedShop({data: {images}}) {
   return <Layout header={'Back to Black'}>
     <Nod title={'Back to Black'}/>
-    <p>
+    <p className="h5 general-text">
       Back to black. Its essence is deeply connected with the theme of the
       journey; starting from finding it, a special treasure hunt organized to
       celebrate the opening!
@@ -16,67 +16,67 @@ export default function TheStripedShop({data: {images}}) {
     <div className="row  justify-content-center project-gallery">
       <div className="col-12 col-md-4">
         <GatsbyImage
-            image={getImage(images.nodes[3])}/>
+            image={getImage(images.nodes[3])} alt=""/>
       </div>
       <div className="col-12 col-md-4">
         <GatsbyImage
-            image={getImage(images.nodes[5])}/>
+            image={getImage(images.nodes[5])} alt=""/>
       </div>
       <div className="col-12 col-md-4">
         <GatsbyImage
-            image={getImage(images.nodes[13])}/>
+            image={getImage(images.nodes[13])} alt=""/>
       </div>
       <div className="col-12 col-md-6">
         <GatsbyImage
-            image={getImage(images.nodes[1])}/>
-      </div>
-
-      <div className="col-12 col-md-6">
-        <GatsbyImage
-            image={getImage(images.nodes[6])}/>
-      </div>
-
-      <div className="col-12 col-md-4">
-        <GatsbyImage
-            image={getImage(images.nodes[10])}/>
-      </div>
-
-      <div className="col-12 col-md-4">
-        <GatsbyImage
-            image={getImage(images.nodes[7])}/>
-      </div>
-
-      <div className="col-12 col-md-4">
-        <GatsbyImage
-            image={getImage(images.nodes[2])}/>
+            image={getImage(images.nodes[1])} alt=""/>
       </div>
 
       <div className="col-12 col-md-6">
         <GatsbyImage
-            image={getImage(images.nodes[8])}/>
+            image={getImage(images.nodes[6])} alt=""/>
+      </div>
+
+      <div className="col-12 col-md-4">
+        <GatsbyImage
+            image={getImage(images.nodes[10])} alt=""/>
+      </div>
+
+      <div className="col-12 col-md-4">
+        <GatsbyImage
+            image={getImage(images.nodes[7])} alt=""/>
+      </div>
+
+      <div className="col-12 col-md-4">
+        <GatsbyImage
+            image={getImage(images.nodes[2])} alt=""/>
+      </div>
+
+      <div className="col-12 col-md-6">
+        <GatsbyImage
+            image={getImage(images.nodes[8])} alt=""/>
       </div>
       <div className="col-12 col-md-6">
         <GatsbyImage
-            image={getImage(images.nodes[13])}/>
+            image={getImage(images.nodes[13])} alt=""/>
       </div>
 
       <div className="col-12 col-md-4">
         <GatsbyImage
-            image={getImage(images.nodes[4])}/>
+            image={getImage(images.nodes[4])} alt=""/>
       </div>
 
       <div className="col-12 col-md-4">
         <GatsbyImage
-            image={getImage(images.nodes[12])}/>
+            image={getImage(images.nodes[12])} alt=""/>
       </div>
 
       <div className="col-12 col-md-4">
         <GatsbyImage
-            image={getImage(images.nodes[14])}/>
+            image={getImage(images.nodes[14])} alt=""/>
       </div>
       <div className="col-12 col-md-4">
         <GatsbyImage
-            image={getImage(images.nodes[13])}/>
+            image={getImage(images.nodes[13])} alt=""/>
       </div>
     </div>
   </Layout>;
