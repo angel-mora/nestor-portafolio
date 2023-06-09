d7e26af1b09c2db50c818a0bd718f4d2db4a5c26
vicma <vmaldaraca@gmail.com>
2023-06-09 15:21:49 -0600

    Fix lookbook component and clean style.scss

--- src/pages/lookbook.js
+++ src/pages/lookbook.js
@@ -28,39 +28,37 @@ export default function SanBenedetto({data: {images}}) {
     </div>
 
     <div className="row project-gallery" id="fc-pics-container">
-      <div className="col-6">
+      <div className="col-xl-6 col-md-6 col-lg-6 col-sm-12 col-xs-6">
         <GatsbyImage image={getImage(images.nodes[5])} alt="lookbook-image" />
       </div>
-      <div className="col-6">
+      <div className="col-xl-6 col-md-6 col-lg-6 col-sm-12 col-xs-6">
         <GatsbyImage image={getImage(images.nodes[9])} alt="lookbook-image" />
       </div>
     </div>
 
     <div className="row project-gallery" id="fc-pics-container">
-      <div id="fc1">
+      <div className="col-xl-4 col-lg-4 col-md-6 col-sm-6 col-xs-6">
         <GatsbyImage image={getImage(images.nodes[13])} alt="lookbook-image" />
       </div>
-      <div id="fc2">
+      <div className="col-xl-4 col-lg-4 col-md-6 col-sm-6 col-xs-6">
         <GatsbyImage image={getImage(images.nodes[15])} alt="lookbook-image" />
       </div>
-      <div id="fc3">
+      <div className="col-xl-4 col-lg-4 col-md-6 col-sm-6 col-xs-6">
         <GatsbyImage image={getImage(images.nodes[16])} alt="lookbook-image" />
       </div>
     </div>
 
-    <div className="project-gallery">
-      <div className="row ">
-        <div className="col-4">
+    <div className="row project-gallery" id="fc-pics-container" >
+        <div className="col-xl-4 col-lg-4 col-md-6 col-sm-6 col-xs-6">
           <GatsbyImage image={getImage(images.nodes[10])} alt="lookbook-image"/>
         </div>
-        <div className="col-4">
+        <div className="col-xl-4 col-lg-4 col-md-6 col-sm-6 col-xs-6">
           <GatsbyImage image={getImage(images.nodes[11])} alt="lookbook-image"/>
         </div>
-        <div className="col-4">
+        <div className="col-xl-4 col-lg-4 col-md-6 col-sm-6 col-xs-6">
             <GatsbyImage image={getImage(images.nodes[12])} alt="lookbook-image"/>
         </div>
       </div>
-    </div>
   </Layout>;
 }
 export const query = graphql`{
